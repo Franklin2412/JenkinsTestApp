9779b303f2ebb89b6e000cdb695234205f4b905d
Franklin Michael <franklin.michael@FranklinMichael.local>
2016-06-04 11:01:05 +0530

    jenkins test

--- app/src/main/java/com/laminin/franklinmichael/junitjenkinstestapp/MathUtils.java
+++ app/src/main/java/com/laminin/franklinmichael/junitjenkinstestapp/MathUtils.java
@@ -7,7 +7,7 @@ package com.laminin.franklinmichael.junitjenkinstestapp;
 public class MathUtils {
 
     public int add (int a, int b){
-        return a + b;
+        return a + b ;
     }
 
     public int subtract (int a, int b){
